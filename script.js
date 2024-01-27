b02144b32ff512f2c4bef1f1ecabd0ab71eeb20d
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-01-27 22:39:22 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Accordion Functionality
+const questionBox = document.querySelectorAll('.question-box');
+
+questionBox.forEach((qbox) => {
+  qbox.addEventListener('click', function () {
+    qbox.classList.toggle('open');
+
+    const svgEl = qbox.querySelector('svg');
+
+    if (qbox.classList.contains('open')) {
+      svgEl.innerHTML =
+        '<path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/>';
+    } else {
+      svgEl.innerHTML = svgEl.innerHTML =
+        '<path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/>';
+    }
+  });
+});
+
+// NOTES: Remember we can select elements not only from document.querySelector but also starting from a specific element like in this example qbox.querySelector => only the svg element child of the current question box element.
